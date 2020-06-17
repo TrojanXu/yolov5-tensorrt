ed8d7a56291b8013b4ccc6c41483db09be766502
TrojanXu <djgdskmkcmm@163.com>
2020-06-17 11:04:09 +0800

    add trt code

--- main.py
+++ main.py
@@ -4,33 +4,62 @@ sys.path.append('yolov5')
 from models.common import *
 from utils.torch_utils import *
 from yolo import *
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+
+device = torch_utils.select_device('0')
+weights = 'yolov5s.pt'
 
 
 def load_model():
-    device = torch_utils.select_device('0')
-    weights = 'yolov5s.pt'
     # Load model
     #model = torch.load(weights, map_location=device)['model']
     model = Model('yolov5s.yaml').to(device)
     ckpt = torch.load(weights, map_location=device)
     ckpt['model'] = \
                 {k: v for k, v in ckpt['model'].state_dict().items() if model.state_dict()[k].numel() == v.numel()}
-    model.load_state_dict(ckpt['model'])#, strict=False)
-    # torch.save(torch.load(weights, map_location=device), weights)  # update model if SourceChangeWarning
-    # model.fuse()
+    model.load_state_dict(ckpt['model'], strict=False)
     model.eval()
+    return model
 
-    img = torch.zeros((32, 3, 640, 640)).to(device)
-    #_ = model(img)
-    #m = ckpt['model']
-    #names = m.names if hasattr(m, 'names') else m.modules.names
-    #colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+def export_onnx(model):
+    img = torch.zeros((1, 3, 640, 640)).to(device)
     torch.onnx.export(model, (img), 'yolov5.onnx', 
            input_names=["data"], output_names=["model/22"], verbose=True, opset_version=10, operator_export_type=torch.onnx.OperatorExportTypes.ONNX
     )
 
-    print(model(img))
+
+def build_engine(onnx_path):
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+        builder.max_batch_size = 1 # always 1 for explicit batch
+        config = builder.create_builder_config()
+        config.max_workspace_size = GiB(1)
+        # Load the Onnx model and parse it in order to populate the TensorRT network.
+        with open(onnx_path, 'rb') as model:
+            if not parser.parse(model.read()):
+                print ('ERROR: Failed to parse the ONNX file.')
+                for error in range(parser.num_errors):
+                    print (parser.get_error(error))
+                return None
+        return builder.build_engine(network, config)
+
+
+def profile_trt(engine):
+    pass
+
+
+def profile_torch(model):
+    pass
 
 
 if __name__ == '__main__':
-    load_model()
+    model = load_model()
+    export_onnx(model)
+    engine = build_engine('yolov5.onnx')
+    assert(engine is not None)
+
+    profile_trt(engine)
+    profile_torch(model)
+    
